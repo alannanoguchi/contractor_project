f6909ad94ca7cfe27211254592ca4843ece1abd4
Alanna <alanna.noguchi@students.makeschool.com>
2019-10-07 20:31:27 -0700

    Added base.html and home.html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+# from pymongo import MangoClient
 
 app = Flask(__name__)
+# client = MangoClient()
+# db = client.Contractor
+# contractor = db.contractor
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return 'Hello, world!'
+    return render_template('home.html', msg='Homepage')
 
 if __name__ == '__main__':
     app.run(debug=True)
