67e2f01ac858e57c53ed970d0e9b5a02ce1e9375
Alanna <alanna.noguchi@students.makeschool.com>
2019-10-07 21:07:32 -0700

    Added MongoDB

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template
-# from pymongo import MangoClient
+from pymongo import MangoClient
 
 app = Flask(__name__)
-# client = MangoClient()
-# db = client.Contractor
-# contractor = db.contractor
+client = MangoClient()
+db = client.Contractor
+contractor = db.contractor
 
 @app.route('/')
 def index():
     """Return homepage."""
     return render_template('home.html', msg='Homepage')
 
-collections = [
-    { 'title': 'Causual', 'description': 'Bracelets for everyday, casual wear.' },
-    { 'title': 'Sale', 'description': 'Bracelets on sale!' }
-]
 
 @app.route('/')
 def collections_index():
     """Show all items in collection"""
-    return render_template('collections_index.html', collections=collections)
+    return render_template('collections_index.html', collections=collections.find())
 
 
 if __name__ == '__main__':
